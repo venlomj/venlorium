5eccd4f5296b12b82383fcf43025c7d4607610e3
venlomj <venlo.mj@hotmail.nl>
2025-01-16 17:58:41 +0100

    adding models

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -4,14 +4,29 @@ namespace App\Controllers;
 
 use App\Models\User;
 use Lib\Core\BaseController;
+use Lib\Database\QB;
+use Lib\HTTP\Response;
 
 class AuthController extends BaseController {
-    public function index() {
-        // Fetch users without loading roles
-        $users = User::with(['roles'])->select()->get();
+    // public function index() {
+    //     // Fetch users without loading roles
+    //     $users = User::with(['roles'])->select()->get();
     
-        // Return a JSON response
-        return json_encode([
+    //     // Return a JSON response
+    //     return json_encode([
+    //         "message" => "Hello World",
+    //         "users" => $users
+    //     ]);
+    // }
+
+    public function index()
+    {
+        $users = QB::table("users")
+            ->select('id, name')
+            //->where('name', '=', 'Murrel')
+            ->get();
+
+        return Response::json([
             "message" => "Hello World",
             "users" => $users
         ]);
